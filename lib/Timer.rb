29120da488dea293cec2de58a84e83d22b646d9a
rickstewart <rick.stewart@ns.sympatico.ca>
2015-01-23 19:35:21 -0400

    adding files

--- lib/Timer.rb
+++ lib/Timer.rb
@@ -5,25 +5,7 @@ class Timer
     @seconds = 0
   end
 
-  def time_string(num_of_sec=0)
-    @seconds=num_of_sec
-    "00:00:#{num_of_sec}"
-  end
-
-  def hours_min_sec(time_in_seconds)
-    hours = time_in_seconds / 126000
-    remainder = time_in_seconds - (hours * 126000)
-    minutes = remainder / 3600
-    remainder = remainder - (minutes * 3600)
-    seconds = remainder
-    puts hours
-    puts minutes
-    puts seconds
+  def time_string
+    Time.at(@seconds).utc.strftime("%H:%M:%S")
   end
 end
-
-timer = Timer.new
-timer.hours_min_sec(20)
-timer.hours_min_sec(61)
-timer.hours_min_sec(3200)
-timer.hours_min_sec(6000)
